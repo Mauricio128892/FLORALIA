3ff97be2fc509ab008bf9626be26edd4af8c66bb
AndresHeredia-star <herediaandres8537@gmail.com>
2025-04-09 11:02:08 -0600

    Add files via upload

---
+++ MENU_PRINCIPAL/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MENU_PRINCIPAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1_agregarclientenuevo = new System.Windows.Forms.Button();
+            this.button2_generarpedido = new System.Windows.Forms.Button();
+            this.button3_recomendaciones = new System.Windows.Forms.Button();
+            this.button4_inventario = new System.Windows.Forms.Button();
+            this.button5_historialdepedidos = new System.Windows.Forms.Button();
+            this.label1_bienvenido = new System.Windows.Forms.Label();
+            this.pictureBox1_logotipo = new System.Windows.Forms.PictureBox();
+            this.button6_cerrarsesion = new System.Windows.Forms.Button();
+            this.button7_informacion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_logotipo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1_agregarclientenuevo
+            // 
+            this.button1_agregarclientenuevo.BackColor = System.Drawing.Color.LightPink;
+            this.button1_agregarclientenuevo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1_agregarclientenuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1_agregarclientenuevo.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_agregarclientenuevo.Location = new System.Drawing.Point(54, 71);
+            this.button1_agregarclientenuevo.Name = "button1_agregarclientenuevo";
+            this.button1_agregarclientenuevo.Size = new System.Drawing.Size(192, 41);
+            this.button1_agregarclientenuevo.TabIndex = 0;
+            this.button1_agregarclientenuevo.Text = "Agregar nuevo cliente";
+            this.button1_agregarclientenuevo.UseVisualStyleBackColor = false;
+            // 
+            // button2_generarpedido
+            // 
+            this.button2_generarpedido.BackColor = System.Drawing.Color.LightPink;
+            this.button2_generarpedido.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2_generarpedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2_generarpedido.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_generarpedido.Location = new System.Drawing.Point(54, 134);
+            this.button2_generarpedido.Name = "button2_generarpedido";
+            this.button2_generarpedido.Size = new System.Drawing.Size(192, 41);
+            this.button2_generarpedido.TabIndex = 1;
+            this.button2_generarpedido.Text = "Generar pedido";
+            this.button2_generarpedido.UseVisualStyleBackColor = false;
+            // 
+            // button3_recomendaciones
+            // 
+            this.button3_recomendaciones.BackColor = System.Drawing.Color.LightPink;
+            this.button3_recomendaciones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3_recomendaciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3_recomendaciones.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_recomendaciones.Location = new System.Drawing.Point(54, 197);
+            this.button3_recomendaciones.Name = "button3_recomendaciones";
+            this.button3_recomendaciones.Size = new System.Drawing.Size(192, 41);
+            this.button3_recomendaciones.TabIndex = 2;
+            this.button3_recomendaciones.Text = "Recomendaciones";
+            this.button3_recomendaciones.UseVisualStyleBackColor = false;
+            // 
+            // button4_inventario
+            // 
+            this.button4_inventario.BackColor = System.Drawing.Color.LightPink;
+            this.button4_inventario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4_inventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4_inventario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4_inventario.Location = new System.Drawing.Point(54, 262);
+            this.button4_inventario.Name = "button4_inventario";
+            this.button4_inventario.Size = new System.Drawing.Size(192, 41);
+            this.button4_inventario.TabIndex = 3;
+            this.button4_inventario.Text = "Inventario";
+            this.button4_inventario.UseVisualStyleBackColor = false;
+            // 
+            // button5_historialdepedidos
+            // 
+            this.button5_historialdepedidos.BackColor = System.Drawing.Color.LightPink;
+            this.button5_historialdepedidos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5_historialdepedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5_historialdepedidos.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5_historialdepedidos.Location = new System.Drawing.Point(54, 324);
+            this.button5_historialdepedidos.Name = "button5_historialdepedidos";
+            this.button5_historialdepedidos.Size = new System.Drawing.Size(192, 41);
+            this.button5_historialdepedidos.TabIndex = 4;
+            this.button5_historialdepedidos.Text = "Historial de pedidos";
+            this.button5_historialdepedidos.UseVisualStyleBackColor = false;
+            // 
+            // label1_bienvenido
+            // 
+            this.label1_bienvenido.AutoSize = true;
+            this.label1_bienvenido.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1_bienvenido.ForeColor = System.Drawing.Color.HotPink;
+            this.label1_bienvenido.Location = new System.Drawing.Point(347, 325);
+            this.label1_bienvenido.Name = "label1_bienvenido";
+            this.label1_bienvenido.Size = new System.Drawing.Size(359, 40);
+            this.label1_bienvenido.TabIndex = 5;
+            this.label1_bienvenido.Text = "Bienvenido a Floralia";
+            // 
+            // pictureBox1_logotipo
+            // 
+            this.pictureBox1_logotipo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1_logotipo.Image")));
+            this.pictureBox1_logotipo.Location = new System.Drawing.Point(386, 96);
+            this.pictureBox1_logotipo.Name = "pictureBox1_logotipo";
+            this.pictureBox1_logotipo.Size = new System.Drawing.Size(286, 209);
+            this.pictureBox1_logotipo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1_logotipo.TabIndex = 6;
+            this.pictureBox1_logotipo.TabStop = false;
+            // 
+            // button6_cerrarsesion
+            // 
+            this.button6_cerrarsesion.BackColor = System.Drawing.Color.LightPink;
+            this.button6_cerrarsesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6_cerrarsesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6_cerrarsesion.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6_cerrarsesion.Location = new System.Drawing.Point(669, 408);
+            this.button6_cerrarsesion.Name = "button6_cerrarsesion";
+            this.button6_cerrarsesion.Size = new System.Drawing.Size(120, 30);
+            this.button6_cerrarsesion.TabIndex = 7;
+            this.button6_cerrarsesion.Text = "Cerrar sesion";
+            this.button6_cerrarsesion.UseVisualStyleBackColor = false;
+            // 
+            // button7_informacion
+            // 
+            this.button7_informacion.BackColor = System.Drawing.Color.LightPink;
+            this.button7_informacion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7_informacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7_informacion.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7_informacion.ForeColor = System.Drawing.Color.Black;
+            this.button7_informacion.Location = new System.Drawing.Point(669, 13);
+            this.button7_informacion.Name = "button7_informacion";
+            this.button7_informacion.Size = new System.Drawing.Size(119, 34);
+            this.button7_informacion.TabIndex = 8;
+            this.button7_informacion.Text = "Informacion";
+            this.button7_informacion.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7_informacion);
+            this.Controls.Add(this.button6_cerrarsesion);
+            this.Controls.Add(this.pictureBox1_logotipo);
+            this.Controls.Add(this.label1_bienvenido);
+            this.Controls.Add(this.button5_historialdepedidos);
+            this.Controls.Add(this.button4_inventario);
+            this.Controls.Add(this.button3_recomendaciones);
+            this.Controls.Add(this.button2_generarpedido);
+            this.Controls.Add(this.button1_agregarclientenuevo);
+            this.Name = "Form1";
+            this.Text = "Floralia";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_logotipo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_agregarclientenuevo;
+        private System.Windows.Forms.Button button2_generarpedido;
+        private System.Windows.Forms.Button button3_recomendaciones;
+        private System.Windows.Forms.Button button4_inventario;
+        private System.Windows.Forms.Button button5_historialdepedidos;
+        private System.Windows.Forms.Label label1_bienvenido;
+        private System.Windows.Forms.PictureBox pictureBox1_logotipo;
+        private System.Windows.Forms.Button button6_cerrarsesion;
+        private System.Windows.Forms.Button button7_informacion;
+    }
+}
+
---
+++ MENU_PRINCIPAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("MENU_PRINCIPAL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MENU_PRINCIPAL")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4230d744-60ac-4475-af8d-3c9adb7d15e3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

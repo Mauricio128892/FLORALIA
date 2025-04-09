0d4d964b4023daa452e39067516badc2dfa1b768
AndresHeredia-star <herediaandres8537@gmail.com>
2025-04-09 11:06:30 -0600

    Add files via upload

---
+++ recomendaciones_v2/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace recomendaciones_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1_recomendaciones = new System.Windows.Forms.GroupBox();
+            this.pictureBox1_cumpleaños = new System.Windows.Forms.PictureBox();
+            this.pictureBox2_bodas = new System.Windows.Forms.PictureBox();
+            this.pictureBox3_eventos = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1_comprarcumpleaños = new System.Windows.Forms.Button();
+            this.button2_comprarbodas = new System.Windows.Forms.Button();
+            this.button3_comprareventos = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1_recomendaciones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_cumpleaños)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_bodas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3_eventos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1_recomendaciones
+            // 
+            this.groupBox1_recomendaciones.Controls.Add(this.label9);
+            this.groupBox1_recomendaciones.Controls.Add(this.label8);
+            this.groupBox1_recomendaciones.Controls.Add(this.label7);
+            this.groupBox1_recomendaciones.Controls.Add(this.button3_comprareventos);
+            this.groupBox1_recomendaciones.Controls.Add(this.button2_comprarbodas);
+            this.groupBox1_recomendaciones.Controls.Add(this.button1_comprarcumpleaños);
+            this.groupBox1_recomendaciones.Controls.Add(this.label6);
+            this.groupBox1_recomendaciones.Controls.Add(this.label5);
+            this.groupBox1_recomendaciones.Controls.Add(this.label4);
+            this.groupBox1_recomendaciones.Controls.Add(this.label3);
+            this.groupBox1_recomendaciones.Controls.Add(this.label2);
+            this.groupBox1_recomendaciones.Controls.Add(this.label1);
+            this.groupBox1_recomendaciones.Controls.Add(this.pictureBox3_eventos);
+            this.groupBox1_recomendaciones.Controls.Add(this.pictureBox2_bodas);
+            this.groupBox1_recomendaciones.Controls.Add(this.pictureBox1_cumpleaños);
+            this.groupBox1_recomendaciones.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1_recomendaciones.ForeColor = System.Drawing.Color.HotPink;
+            this.groupBox1_recomendaciones.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1_recomendaciones.Name = "groupBox1_recomendaciones";
+            this.groupBox1_recomendaciones.Size = new System.Drawing.Size(775, 425);
+            this.groupBox1_recomendaciones.TabIndex = 0;
+            this.groupBox1_recomendaciones.TabStop = false;
+            this.groupBox1_recomendaciones.Text = "Recomendaciones";
+            this.groupBox1_recomendaciones.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pictureBox1_cumpleaños
+            // 
+            this.pictureBox1_cumpleaños.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1_cumpleaños.Image")));
+            this.pictureBox1_cumpleaños.Location = new System.Drawing.Point(58, 83);
+            this.pictureBox1_cumpleaños.Name = "pictureBox1_cumpleaños";
+            this.pictureBox1_cumpleaños.Size = new System.Drawing.Size(188, 140);
+            this.pictureBox1_cumpleaños.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1_cumpleaños.TabIndex = 0;
+            this.pictureBox1_cumpleaños.TabStop = false;
+            // 
+            // pictureBox2_bodas
+            // 
+            this.pictureBox2_bodas.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2_bodas.Image")));
+            this.pictureBox2_bodas.Location = new System.Drawing.Point(288, 83);
+            this.pictureBox2_bodas.Name = "pictureBox2_bodas";
+            this.pictureBox2_bodas.Size = new System.Drawing.Size(188, 140);
+            this.pictureBox2_bodas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2_bodas.TabIndex = 1;
+            this.pictureBox2_bodas.TabStop = false;
+            // 
+            // pictureBox3_eventos
+            // 
+            this.pictureBox3_eventos.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3_eventos.Image")));
+            this.pictureBox3_eventos.Location = new System.Drawing.Point(518, 83);
+            this.pictureBox3_eventos.Name = "pictureBox3_eventos";
+            this.pictureBox3_eventos.Size = new System.Drawing.Size(188, 140);
+            this.pictureBox3_eventos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3_eventos.TabIndex = 2;
+            this.pictureBox3_eventos.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(105, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cumpleaños";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DeepPink;
+            this.label2.Location = new System.Drawing.Point(358, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bodas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DeepPink;
+            this.label3.Location = new System.Drawing.Point(581, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Eventos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DeepPink;
+            this.label4.Location = new System.Drawing.Point(54, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 80);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Rosas en tonos rosa y rojo\r\nGirasoles\r\nLirios \r\nMargaritas blancas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DeepPink;
+            this.label5.Location = new System.Drawing.Point(296, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 80);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Rosas blancas\r\nPeonías rosas\r\nLirios blancos blancos\r\nPaniculata";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DeepPink;
+            this.label6.Location = new System.Drawing.Point(536, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 80);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Rosas rojas intensas\r\nHortensias azul \r\nOrquídeas doradas\r\nLirios cala blancos\r\n";
+            // 
+            // button1_comprarcumpleaños
+            // 
+            this.button1_comprarcumpleaños.BackColor = System.Drawing.Color.LightPink;
+            this.button1_comprarcumpleaños.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1_comprarcumpleaños.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1_comprarcumpleaños.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_comprarcumpleaños.ForeColor = System.Drawing.Color.Black;
+            this.button1_comprarcumpleaños.Location = new System.Drawing.Point(58, 390);
+            this.button1_comprarcumpleaños.Name = "button1_comprarcumpleaños";
+            this.button1_comprarcumpleaños.Size = new System.Drawing.Size(128, 29);
+            this.button1_comprarcumpleaños.TabIndex = 9;
+            this.button1_comprarcumpleaños.Text = "C O M P R A R";
+            this.button1_comprarcumpleaños.UseVisualStyleBackColor = false;
+            // 
+            // button2_comprarbodas
+            // 
+            this.button2_comprarbodas.BackColor = System.Drawing.Color.LightPink;
+            this.button2_comprarbodas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2_comprarbodas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2_comprarbodas.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_comprarbodas.ForeColor = System.Drawing.Color.Black;
+            this.button2_comprarbodas.Location = new System.Drawing.Point(300, 390);
+            this.button2_comprarbodas.Name = "button2_comprarbodas";
+            this.button2_comprarbodas.Size = new System.Drawing.Size(128, 29);
+            this.button2_comprarbodas.TabIndex = 10;
+            this.button2_comprarbodas.Text = "C O M P R A R";
+            this.button2_comprarbodas.UseVisualStyleBackColor = false;
+            // 
+            // button3_comprareventos
+            // 
+            this.button3_comprareventos.BackColor = System.Drawing.Color.LightPink;
+            this.button3_comprareventos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3_comprareventos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3_comprareventos.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_comprareventos.ForeColor = System.Drawing.Color.Black;
+            this.button3_comprareventos.Location = new System.Drawing.Point(549, 390);
+            this.button3_comprareventos.Name = "button3_comprareventos";
+            this.button3_comprareventos.Size = new System.Drawing.Size(128, 29);
+            this.button3_comprareventos.TabIndex = 11;
+            this.button3_comprareventos.Text = "C O M P R A R";
+            this.button3_comprareventos.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(93, 367);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "$ 3 0 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(334, 368);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "$ 4 8 0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(581, 368);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 18);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "$ 5 5 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1_recomendaciones);
+            this.Name = "Form1";
+            this.Text = "Floralia";
+            this.groupBox1_recomendaciones.ResumeLayout(false);
+            this.groupBox1_recomendaciones.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_cumpleaños)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_bodas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3_eventos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1_recomendaciones;
+        private System.Windows.Forms.PictureBox pictureBox3_eventos;
+        private System.Windows.Forms.PictureBox pictureBox2_bodas;
+        private System.Windows.Forms.PictureBox pictureBox1_cumpleaños;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3_comprareventos;
+        private System.Windows.Forms.Button button2_comprarbodas;
+        private System.Windows.Forms.Button button1_comprarcumpleaños;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+

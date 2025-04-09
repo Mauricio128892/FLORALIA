c6ed434535313662e7d3b113510808f9a2f0938f
AndresHeredia-star <herediaandres8537@gmail.com>
2025-04-09 10:56:31 -0600

    Add files via upload

---
+++ INFORMACION_TIENDA/INFORMACIÓN_TIENDA.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace INFORMACION_TIENDA
+{
+    partial class INFORMACIÓN_TIENDA
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(INFORMACIÓN_TIENDA));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_editarInformacion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3_Correoinfo = new System.Windows.Forms.TextBox();
+            this.textBox2_Telefonoinfo = new System.Windows.Forms.TextBox();
+            this.textBox1_Direccioninfo = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_editarInformacion);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3_Correoinfo);
+            this.groupBox1.Controls.Add(this.textBox2_Telefonoinfo);
+            this.groupBox1.Controls.Add(this.textBox1_Direccioninfo);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.HotPink;
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FLORALIA";
+            // 
+            // button_editarInformacion
+            // 
+            this.button_editarInformacion.BackColor = System.Drawing.Color.LightPink;
+            this.button_editarInformacion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_editarInformacion.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_editarInformacion.ForeColor = System.Drawing.Color.Black;
+            this.button_editarInformacion.Location = new System.Drawing.Point(625, 0);
+            this.button_editarInformacion.Name = "button_editarInformacion";
+            this.button_editarInformacion.Size = new System.Drawing.Size(144, 30);
+            this.button_editarInformacion.TabIndex = 9;
+            this.button_editarInformacion.Text = "Editar información";
+            this.button_editarInformacion.UseVisualStyleBackColor = false;
+            this.button_editarInformacion.Click += new System.EventHandler(this.button_editarInformacion_Click);
+            this.button_editarInformacion.Paint += new System.Windows.Forms.PaintEventHandler(this.button_editarInformacion_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(357, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 173);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(338, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(360, 168);
+            this.label4.TabIndex = 7;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // textBox3_Correoinfo
+            // 
+            this.textBox3_Correoinfo.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox3_Correoinfo.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3_Correoinfo.Location = new System.Drawing.Point(10, 368);
+            this.textBox3_Correoinfo.Name = "textBox3_Correoinfo";
+            this.textBox3_Correoinfo.ReadOnly = true;
+            this.textBox3_Correoinfo.Size = new System.Drawing.Size(223, 28);
+            this.textBox3_Correoinfo.TabIndex = 6;
+            this.textBox3_Correoinfo.Text = "floralia116@gmail.com";
+            // 
+            // textBox2_Telefonoinfo
+            // 
+            this.textBox2_Telefonoinfo.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox2_Telefonoinfo.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_Telefonoinfo.Location = new System.Drawing.Point(10, 269);
+            this.textBox2_Telefonoinfo.Name = "textBox2_Telefonoinfo";
+            this.textBox2_Telefonoinfo.ReadOnly = true;
+            this.textBox2_Telefonoinfo.Size = new System.Drawing.Size(126, 28);
+            this.textBox2_Telefonoinfo.TabIndex = 5;
+            this.textBox2_Telefonoinfo.Text = "9811876147";
+            this.textBox2_Telefonoinfo.TextChanged += new System.EventHandler(this.textBox2_Telefonoinfo_TextChanged);
+            // 
+            // textBox1_Direccioninfo
+            // 
+            this.textBox1_Direccioninfo.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox1_Direccioninfo.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_Direccioninfo.Location = new System.Drawing.Point(10, 123);
+            this.textBox1_Direccioninfo.Multiline = true;
+            this.textBox1_Direccioninfo.Name = "textBox1_Direccioninfo";
+            this.textBox1_Direccioninfo.ReadOnly = true;
+            this.textBox1_Direccioninfo.Size = new System.Drawing.Size(223, 98);
+            this.textBox1_Direccioninfo.TabIndex = 4;
+            this.textBox1_Direccioninfo.Text = "Av. Resurgimiento 611, Bosques de Campeche, \r\n24030 San Francisco de Campeche, Ca" +
+    "mp.";
+            this.textBox1_Direccioninfo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(14, 109);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 40);
+            this.linkLabel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Correo electrónico:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Teléfono:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dirección:";
+            // 
+            // INFORMACIÓN_TIENDA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "INFORMACIÓN_TIENDA";
+            this.Text = "INFORMACION ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1_Direccioninfo;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox2_Telefonoinfo;
+        private System.Windows.Forms.TextBox textBox3_Correoinfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_editarInformacion;
+    }
+}
+
---
+++ INFORMACION_TIENDA/INFORMACIÓN_TIENDA.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INFORMACION_TIENDA
+{
+    public partial class INFORMACIÓN_TIENDA: Form
+    {
+        private bool editando = false;
+        private bool esAdministrador;
+
+        public INFORMACIÓN_TIENDA(bool admin)
+        {
+            InitializeComponent();
+            esAdministrador = admin;
+            button_editarInformacion.Enabled = esAdministrador;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_editarInformacion_Click(object sender, EventArgs e)
+        {
+            // Cambia el estado de edición
+            editando = !editando;
+
+            // Habilita o deshabilita los TextBox según el estado de edición
+            textBox1_Direccioninfo.ReadOnly = !editando;
+            textBox2_Telefonoinfo.ReadOnly = !editando;
+            textBox3_Correoinfo.ReadOnly = !editando;
+
+            // Cambia el texto del botón según el estado
+            button_editarInformacion.Text = editando ? "Guardar" : "Editar información";
+        }
+
+        private void button_editarInformacion_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        private void textBox2_Telefonoinfo_TextChanged(object sender, EventArgs e)
+        {
+            // Validar que solo sean números
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox2_Telefonoinfo.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Solo se permiten números en el teléfono.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox2_Telefonoinfo.Text = System.Text.RegularExpressions.Regex.Replace(textBox2_Telefonoinfo.Text, "[^0-9]", "");
+                textBox2_Telefonoinfo.SelectionStart = textBox2_Telefonoinfo.Text.Length; // Mantiene el cursor al final
+            }
+
+            // Limitar a 10 caracteres
+            if (textBox2_Telefonoinfo.Text.Length > 10)
+            {
+                textBox2_Telefonoinfo.Text = textBox2_Telefonoinfo.Text.Substring(0, 10);
+                textBox2_Telefonoinfo.SelectionStart = textBox2_Telefonoinfo.Text.Length; // Mantiene el cursor al final
+            }
+        }
+    }
+}
